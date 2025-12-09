57584bfbefe6b704bf1583f7e797cfed733b8630
Casal <javierlolcasal@gmail.com>
2025-12-09 13:50:44 +0100

    feat: Creacion de proyecto

---
+++ Flota/tablero.py
@@ -0,0 +1,19 @@
+class Tablero:
+
+    def __init__(self, tamano):
+
+        self.dimensiones = (tamano, tamano)
+        self.casillas = []
+
+        for fila in range(tamano):
+            fila_actual = []
+            for columna in range(tamano):
+                fila_actual.append(0)
+            self.casillas.append(fila_actual)
+
+    def mostrar_tablero(self):
+
+        print("\nTABLERO:")
+        for fila in self.casillas:
+            print(" ".join(str(casilla) for casilla in fila))
+Añadido un comentario a la clase Tablero.
